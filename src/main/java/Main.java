4937372d577831045037c21e1b9b515aa7ddbae0
TheRealLyonic <rwbyfan2305@gmail.com>
2022-12-22 04:26:30 -0500

    Updated .gitIgnore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,7 @@ import java.io.IOException;
 public class Main{
 
     public static void main(String[] args) throws IOException {
-        //Frame frame = new Frame();
-        ExcelSpreadsheet spreadsheet = new ExcelSpreadsheet("D:\\Documents\\Java Projects\\FinancialManagementSystem\\data\\testInfo.xlsx", "Test");
+        Frame frame = new Frame();
     }
 
 }
