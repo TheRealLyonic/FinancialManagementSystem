4cd875030a21621739528a9c387e450be11fc89b
TheRealLyonic <rwbyfan2305@gmail.com>
2022-12-28 13:54:16 -0500

    Update Date method added to FMS

--- src/main/java/FinancialManagementSystem.java
+++ src/main/java/FinancialManagementSystem.java
@@ -8,6 +8,7 @@ import java.util.Date;
 public class FinancialManagementSystem{
 
     private ExcelSpreadsheet spreadsheet;
+    private double startingBalance;
     private double balance;
     private double spent;
     private int lastDepositRow;
@@ -18,15 +19,15 @@ public class FinancialManagementSystem{
     FinancialManagementSystem() throws IOException {
         spreadsheet = new ExcelSpreadsheet("data\\Financial_Data.xlsx", "Balance Sheet");
 
-        //Date info.
-        localDate = LocalDate.now();
-        currentMonth = localDate.getMonth();
-        currentDay = localDate.getDayOfMonth();
-        currentYear = localDate.getYear();
-
         updateBalance();
         updateLastDepositRow();
         updateTotalSpent();
+        updateCurrentDate();
+
+        System.out.println(localDate);
+        System.out.println(spreadsheet.readFromSpreadsheet(spreadsheet.getLastRow(), 0));
+
+//        if( (spreadsheet.readFromSpreadsheet(spreadsheet.getLastRow(), 0))  )
 
         new UserInterface();
     }
@@ -47,6 +48,13 @@ public class FinancialManagementSystem{
     public void updateTotalSpent() throws IOException {
         //Take the week's paycheck, subtract how much you have left, you get how much you spent
         spent = new BigDecimal(((new BigDecimal(Double.parseDouble(spreadsheet.readFromSpreadsheet(lastDepositRow, 3))).setScale(2, RoundingMode.HALF_EVEN).doubleValue() - balance))).setScale(2, RoundingMode.HALF_EVEN).doubleValue();
+    }
+
+    public void updateCurrentDate() throws IOException{
+        localDate = LocalDate.now();
+        currentMonth = localDate.getMonth();
+        currentDay = localDate.getDayOfMonth();
+        currentYear = localDate.getYear();
     }
 
     //Getters + Setters
