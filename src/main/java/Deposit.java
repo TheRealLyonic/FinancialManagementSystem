77f15d5c495521e6b97ab84c0f4f83a0d7687743
TheRealLyonic <rwbyfan2305@gmail.com>
2023-01-02 16:37:05 -0500

    Continued work on the Deposit class.

--- src/main/java/Deposit.java
+++ src/main/java/Deposit.java
@@ -7,6 +7,8 @@ public class Deposit extends JFrame implements ActionListener, Colors, Fonts{
 
     private JLabel newDepositText;
     private JTextField newDepositTextField;
+    private JButton confirmButton;
+    private JLabel greenDollarIcon;
 
     Deposit(){
         this.setTitle("New Deposit");
@@ -29,12 +31,27 @@ public class Deposit extends JFrame implements ActionListener, Colors, Fonts{
         newDepositTextField.setFont(ROBOTO_MEDIUM);
         newDepositTextField.setLocation(95, 250);
 
+        //Green Dollar Icon stuff
+        greenDollarIcon = new JLabel();
+        greenDollarIcon.setIcon(new ImageIcon("resources\\green_dollar_icon.png"));
+        greenDollarIcon.setSize(95, 95);
+        greenDollarIcon.setLocation(5, 250);
+
+        //Confirm button stuff
+        confirmButton = new JButton("Confirm");
+        confirmButton.setSize(125, 75);
+        confirmButton.setLocation(460, 260);
+        confirmButton.setFont(ROBOTO_BUTTON);
+        confirmButton.setFocusable(false);
+
         //Final JFrame Preparations
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setLocation(this.getX(), 85);
         this.add(newDepositText);
         this.add(newDepositTextField);
+        this.add(greenDollarIcon);
+        this.add(confirmButton);
         this.setVisible(true);
     }
 
